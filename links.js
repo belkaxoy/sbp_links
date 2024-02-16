17751a147c0ff5e098ac3e57f6cbf7f894b6b401
Сорокина Анастасия Олеговна <aosorokina@dev.vtb.ru>
2024-02-16 15:24:20 +0300

    initial commit

---
+++ links.js
@@ -0,0 +1,110 @@
+export const links = {
+  mosOblEirc: {
+    btn_1: {
+      link: 'https://online.vtb.ru/i/paymentSbp/AD10000OVOE0BTQO99Q8LDL6AA5KU5CT?type=02&bank=100000000049&sum=196629',
+      newTab: false,
+    },
+    btn_2: {
+      link: 'https://online.vtb.ru/i/paymentSbp/AD10000OVOE0BTQO99Q8LDL6AA5KU5CT?type=02&bank=100000000049&sum=196629',
+      newTab: true,
+    },
+    btn_3: {
+      link: 'https://online-alpha.vtb.ru/i/paymentSbp/AD10000OVOE0BTQO99Q8LDL6AA5KU5CT?type=02&bank=100000000049&sum=196629&cur=RUB&crc=71DA',
+      newTab: false,
+    },
+    btn_4: {
+      link: 'https://online-alpha.vtb.ru/i/paymentSbp/AD10000OVOE0BTQO99Q8LDL6AA5KU5CT?type=02&bank=100000000049&sum=196629',
+      newTab: true,
+    },
+  },
+  sbp: {
+    btn_1: {
+      link: 'https://online.vtb.ru/i/subscriptionSbp/BB1S007T67G228AB80B87QL7NR7NU3K3',
+      newTab: false,
+    },
+    btn_2: {
+      link: 'https://online.vtb.ru/i/subscriptionSbp/BB1S007T67G228AB80B87QL7NR7NU3K3',
+      newTab: true,
+    },
+    btn_3: {
+      link: 'https://online-alpha.vtb.ru/i/subscriptionSbp/BB1S007T67G228AB80B87QL7NR7NU3K3',
+      newTab: false,
+    },
+    btn_4: {
+      link: 'https://online-alpha.vtb.ru/i/subscriptionSbp/BB1S007T67G228AB80B87QL7NR7NU3K3',
+      newTab: true,
+    },
+  },
+  sbp_pay: {
+    btn_1: {
+      link: 'https://online.vtb.ru/i/paymentSbp/AS1A007S6L54D2GE8BIP92DSJCED7O6M',
+      newTab: false,
+    },
+    btn_2: {
+      link: 'https://online.vtb.ru/i/paymentSbp/AS1A007S6L54D2GE8BIP92DSJCED7O6M',
+      newTab: true,
+    },
+    btn_3: {
+      link: 'https://online-alpha.vtb.ru/i/paymentSbp/AS1A007S6L54D2GE8BIP92DSJCED7O6M',
+      newTab: false,
+    },
+    btn_4: {
+      link: 'https://online-alpha.vtb.ru/i/paymentSbp/AS1A007S6L54D2GE8BIP92DSJCED7O6M',
+      newTab: true,
+    },
+  },
+  sbp_pay_sub: {
+    btn_1: {
+      link: '',
+      newTab: false,
+    },
+    btn_2: {
+      link: '',
+      newTab: true,
+    },
+    btn_3: {
+      link: '',
+      newTab: false,
+    },
+    btn_4: {
+      link: '',
+      newTab: true,
+    },
+  },
+  fns: {
+    btn_1: {
+      link: 'https://online.vtb.ru/payments/external/3142?operationDetails%5BobjectParams%5D%5Bdata%5D=%3C%3Fxml+version%3D%221.0%22+encoding%3D%22UTF-8%22%3F%3E%3CPacketEPD+EDNo%3D%22178300620%22+EDDate%3D%222024-02-12%22+EDAuthor%3D%220000000001%22+EDQuantity%3D%221%22+Sum%3D%221000%22+SystemCode%3D%2299%22+xmlns%3D%22urn%3Acbr-ru%3Aed%3Av2.0%22+xmlns%3Adsig%3D%22urn%3Acbr-ru%3Adsig%3Av1.1%22%3E%3CED101+TransKind%3D%2201%22+Priority%3D%220%22+ReceiptDate%3D%222024-02-12%22+PaytKind%3D%220%22+Sum%3D%221000%22%3E%3Cdsig%3ASigValue%3En%2B9Z7FOO9%2FL2%2Fqf7wRtKhefiwi7rdmdUMRO1CCaQDWTdysMMIgMp9DmFliCWPMhf0Cdb94n54W%2Bixz5Kpjdafg%3D%3D%3C%2Fdsig%3ASigValue%3E%3CPurpose%3E%D0%95%D0%B4%D0%B8%D0%BD%D1%8B%D0%B9+%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%B2%D1%8B%D0%B9+%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%B6%3C%2FPurpose%3E%3CDepartmentalInfo+DrawerStatus%3D%2201%22+CBC%3D%2218201061201010000510%22+OKATO%3D%220%22+PaytReason%3D%220%22+TaxPeriod%3D%220%22+DocNo%3D%2218209966243552313406%22+DocDate%3D%222024-02-12%22+TaxPaytKind%3D%220%22%2F%3E%3CPayer+INN%3D%22616305610903%22%2F%3E%3CPayee+PersonalAcc%3D%2203100643000000018500%22+INN%3D%227727406020%22+KPP%3D%22770801001%22%3E%3CName%3E%D0%9A%D0%B0%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%BE+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8+%28%D0%A4%D0%9D%D0%A1+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8%29%3C%2FName%3E%3CBank+BIC%3D%22017003983%22+CorrespAcc%3D%2240102810445370000059%22%2F%3E%3C%2FPayee%3E%3C%2FED101%3E%3C%2FPacketEPD%3E',
+      newTab: false,
+    },
+    btn_2: {
+      link: 'https://online.vtb.ru/payments/external/3142?operationDetails%5BobjectParams%5D%5Bdata%5D=%3C%3Fxml+version%3D%221.0%22+encoding%3D%22UTF-8%22%3F%3E%3CPacketEPD+EDNo%3D%22178300620%22+EDDate%3D%222024-02-12%22+EDAuthor%3D%220000000001%22+EDQuantity%3D%221%22+Sum%3D%221000%22+SystemCode%3D%2299%22+xmlns%3D%22urn%3Acbr-ru%3Aed%3Av2.0%22+xmlns%3Adsig%3D%22urn%3Acbr-ru%3Adsig%3Av1.1%22%3E%3CED101+TransKind%3D%2201%22+Priority%3D%220%22+ReceiptDate%3D%222024-02-12%22+PaytKind%3D%220%22+Sum%3D%221000%22%3E%3Cdsig%3ASigValue%3En%2B9Z7FOO9%2FL2%2Fqf7wRtKhefiwi7rdmdUMRO1CCaQDWTdysMMIgMp9DmFliCWPMhf0Cdb94n54W%2Bixz5Kpjdafg%3D%3D%3C%2Fdsig%3ASigValue%3E%3CPurpose%3E%D0%95%D0%B4%D0%B8%D0%BD%D1%8B%D0%B9+%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%B2%D1%8B%D0%B9+%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%B6%3C%2FPurpose%3E%3CDepartmentalInfo+DrawerStatus%3D%2201%22+CBC%3D%2218201061201010000510%22+OKATO%3D%220%22+PaytReason%3D%220%22+TaxPeriod%3D%220%22+DocNo%3D%2218209966243552313406%22+DocDate%3D%222024-02-12%22+TaxPaytKind%3D%220%22%2F%3E%3CPayer+INN%3D%22616305610903%22%2F%3E%3CPayee+PersonalAcc%3D%2203100643000000018500%22+INN%3D%227727406020%22+KPP%3D%22770801001%22%3E%3CName%3E%D0%9A%D0%B0%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%BE+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8+%28%D0%A4%D0%9D%D0%A1+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8%29%3C%2FName%3E%3CBank+BIC%3D%22017003983%22+CorrespAcc%3D%2240102810445370000059%22%2F%3E%3C%2FPayee%3E%3C%2FED101%3E%3C%2FPacketEPD%3E',
+      newTab: true,
+    },
+    btn_3: {
+      link: 'https://online-alpha.vtb.ru/payments/external/3142?operationDetails%5BobjectParams%5D%5Bdata%5D=%3C%3Fxml+version%3D%221.0%22+encoding%3D%22UTF-8%22%3F%3E%3CPacketEPD+EDNo%3D%22178300620%22+EDDate%3D%222024-02-12%22+EDAuthor%3D%220000000001%22+EDQuantity%3D%221%22+Sum%3D%221000%22+SystemCode%3D%2299%22+xmlns%3D%22urn%3Acbr-ru%3Aed%3Av2.0%22+xmlns%3Adsig%3D%22urn%3Acbr-ru%3Adsig%3Av1.1%22%3E%3CED101+TransKind%3D%2201%22+Priority%3D%220%22+ReceiptDate%3D%222024-02-12%22+PaytKind%3D%220%22+Sum%3D%221000%22%3E%3Cdsig%3ASigValue%3En%2B9Z7FOO9%2FL2%2Fqf7wRtKhefiwi7rdmdUMRO1CCaQDWTdysMMIgMp9DmFliCWPMhf0Cdb94n54W%2Bixz5Kpjdafg%3D%3D%3C%2Fdsig%3ASigValue%3E%3CPurpose%3E%D0%95%D0%B4%D0%B8%D0%BD%D1%8B%D0%B9+%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%B2%D1%8B%D0%B9+%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%B6%3C%2FPurpose%3E%3CDepartmentalInfo+DrawerStatus%3D%2201%22+CBC%3D%2218201061201010000510%22+OKATO%3D%220%22+PaytReason%3D%220%22+TaxPeriod%3D%220%22+DocNo%3D%2218209966243552313406%22+DocDate%3D%222024-02-12%22+TaxPaytKind%3D%220%22%2F%3E%3CPayer+INN%3D%22616305610903%22%2F%3E%3CPayee+PersonalAcc%3D%2203100643000000018500%22+INN%3D%227727406020%22+KPP%3D%22770801001%22%3E%3CName%3E%D0%9A%D0%B0%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%BE+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8+%28%D0%A4%D0%9D%D0%A1+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8%29%3C%2FName%3E%3CBank+BIC%3D%22017003983%22+CorrespAcc%3D%2240102810445370000059%22%2F%3E%3C%2FPayee%3E%3C%2FED101%3E%3C%2FPacketEPD%3E',
+      newTab: false,
+    },
+    btn_4: {
+      link: 'https://online-alpha.vtb.ru/payments/external/3142?operationDetails%5BobjectParams%5D%5Bdata%5D=%3C%3Fxml+version%3D%221.0%22+encoding%3D%22UTF-8%22%3F%3E%3CPacketEPD+EDNo%3D%22178300620%22+EDDate%3D%222024-02-12%22+EDAuthor%3D%220000000001%22+EDQuantity%3D%221%22+Sum%3D%221000%22+SystemCode%3D%2299%22+xmlns%3D%22urn%3Acbr-ru%3Aed%3Av2.0%22+xmlns%3Adsig%3D%22urn%3Acbr-ru%3Adsig%3Av1.1%22%3E%3CED101+TransKind%3D%2201%22+Priority%3D%220%22+ReceiptDate%3D%222024-02-12%22+PaytKind%3D%220%22+Sum%3D%221000%22%3E%3Cdsig%3ASigValue%3En%2B9Z7FOO9%2FL2%2Fqf7wRtKhefiwi7rdmdUMRO1CCaQDWTdysMMIgMp9DmFliCWPMhf0Cdb94n54W%2Bixz5Kpjdafg%3D%3D%3C%2Fdsig%3ASigValue%3E%3CPurpose%3E%D0%95%D0%B4%D0%B8%D0%BD%D1%8B%D0%B9+%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%B2%D1%8B%D0%B9+%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%B6%3C%2FPurpose%3E%3CDepartmentalInfo+DrawerStatus%3D%2201%22+CBC%3D%2218201061201010000510%22+OKATO%3D%220%22+PaytReason%3D%220%22+TaxPeriod%3D%220%22+DocNo%3D%2218209966243552313406%22+DocDate%3D%222024-02-12%22+TaxPaytKind%3D%220%22%2F%3E%3CPayer+INN%3D%22616305610903%22%2F%3E%3CPayee+PersonalAcc%3D%2203100643000000018500%22+INN%3D%227727406020%22+KPP%3D%22770801001%22%3E%3CName%3E%D0%9A%D0%B0%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%BE+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8+%28%D0%A4%D0%9D%D0%A1+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8%29%3C%2FName%3E%3CBank+BIC%3D%22017003983%22+CorrespAcc%3D%2240102810445370000059%22%2F%3E%3C%2FPayee%3E%3C%2FED101%3E%3C%2FPacketEPD%3E',
+      newTab: true,
+    },
+  },
+  deeplink: {
+    btn_1: {
+      link: 'http://online.vtb.ru/i/kn',
+      newTab: false,
+    },
+    btn_2: {
+      link: 'http://online.vtb.ru/i/kn',
+      newTab: true,
+    },
+    btn_3: {
+      link: 'http://online-alpha.vtb.ru/i/kn',
+      newTab: false,
+    },
+    btn_4: {
+      link: 'http://online-alpha.vtb.ru/i/kn',
+      newTab: true,
+    },
+  },
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+import { links } from "./links.js";
+
+const { mosOblEirc, sbp, sbp_pay, sbp_pay_sub, fns, deeplink } = links;
+
+console.log(mosOblEirc)
